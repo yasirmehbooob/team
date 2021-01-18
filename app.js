77425d849b9958af1928112d14d337597d69b92c
Yasir Mehboob <jdbsflhr17>
2021-01-18 17:25:19 +0500

    commited

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const dotenv = require('dotenv');
 const Sequelize = require('sequelize');
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
+const sequelize = new Sequelize("mydatabase","postgres","",{ dialect: 'postgres'});
 
+app.get('/api/users', (req, res) => {
+    sequelize.findAll().then((err, users) => {
+        if(err) {
+            res.send('error occour' + err);
+        }
+
+        res.send(users);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
